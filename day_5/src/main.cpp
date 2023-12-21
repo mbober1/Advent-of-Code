ff4c603c3df734649f7b4e0b9d4745c6b21a4cca
Marcin Bober <mbober1@gmail.com>
2023-12-21 23:08:00 +0100

    day 5, star 1

--- day_5/src/main.cpp
+++ day_5/src/main.cpp
@@ -19,29 +19,29 @@ std::vector<std::string> read_file(const std::string &filename)
   return results;
 }
 
-enum Entitie
-{
-  SEED,
-  SOIL,
-  FERTILIZER,
-  WATER,
-  LIGHT,
-  TEMPERATURE,
-  HUMIDITY,
-  LOCATION,
-
-  MAX_ENTITIE
-};
-
 struct Database
 {
-  size_t data[MAX_ENTITIE];
+  long int seed;
+  long int soil;
+  long int fertilizer;
+  long int water;
+  long int light;
+  long int temperature;
+  long int humidity;
+  long int location;
+};
+
+struct Map_rule
+{
+  long int range_begin;
+  long int range_end;
+  long int shift;
 };
 
 std::vector<Database> get_seeds(const std::string& line)
 {
   std::vector<Database> results;
-  size_t seed;
+  long int seed;
   std::string header;
   std::istringstream iss(line);
   iss >> header;
@@ -54,22 +54,23 @@ std::vector<Database> get_seeds(const std::string& line)
   return results;
 }
 
-std::map<size_t, size_t> parse_map(const std::vector<std::string>& data, size_t& line)
+std::vector<Map_rule> parse_map(const std::vector<std::string>& data, size_t& line)
 {
-  std::map<size_t, size_t>result;
+  std::vector<Map_rule>result;
   while (false == data[line].empty())
   {
     std::istringstream iss(data[line]);
-    size_t destination_range{0};
-    size_t source_range{0};
-    size_t range_length{0};
+    long int destination_range{0};
+    long int source_range{0};
+    long int range_length{0};
 
     iss >> destination_range >> source_range >> range_length;
 
-    for (size_t i = 0; i < range_length; i++)
-    {
-      result.insert({source_range + i, destination_range + i});
-    }
+    long int range_begin = source_range;
+    long int range_end = source_range + range_length;
+    long int shift = source_range - destination_range;
+
+    result.push_back(Map_rule{range_begin, range_end, shift});
     
     line++;
   }
@@ -78,46 +79,50 @@ std::map<size_t, size_t> parse_map(const std::vector<std::string>& data, size_t&
   return result;
 }
 
-size_t get_from_map(std::map<size_t, size_t> data, size_t key)
+long int get_from_map(const std::vector<Map_rule>& data, long int key)
 {
-  if (data.end() == data.find(key))
+  for (auto &&rule : data)
   {
-    return key;
+    if (key >= rule.range_begin && key <= rule.range_end)
+    {
+      return key - rule.shift;
+    }
   }
-  else
-  {
-    return data.at(key);
-  }
-}
 
+  return key;
+}
 
 
 int main(void)
 {
-  size_t min_location{INT_MAX};
+  long int min_location{INT_MAX};
   auto data = read_file("input");
 
   size_t line{0};
   auto database = get_seeds(data[line]);
   line+=3; // skip
 
-  for (size_t i = 0; i < 1; i++)
-  {
-    auto tmp_map = parse_map(data, line);
-    printf("Map %ld parsed\n", i+1);
-
-    for (auto &&entity : database)
-    {
-      entity.data[i+1] = get_from_map(tmp_map, entity.data[i]);
-    }
-  }
-
+  auto seed_to_soil             = parse_map(data, line);
+  auto soil_to_fertilizer       = parse_map(data, line);
+  auto fertilizer_to_water      = parse_map(data, line);
+  auto water_to_light           = parse_map(data, line);
+  auto light_to_temperature     = parse_map(data, line);
+  auto temperature_to_humidity  = parse_map(data, line);
+  auto humidity_to_location     = parse_map(data, line);
+  
   for (auto &&entity : database)
   {
-    min_location       = std::min(min_location, entity.data[8]);
-    printf("Seed %ld, soil %ld, fertilizer %ld, water %ld, light %ld, temperature %ld, humidity %ld, location %ld.\n", entity.data[SEED], entity.data[SOIL], entity.data[FERTILIZER], entity.data[WATER], entity.data[LIGHT], entity.data[TEMPERATURE], entity.data[HUMIDITY], entity.data[LOCATION]);
+    entity.soil        = get_from_map(seed_to_soil, entity.seed);
+    entity.fertilizer  = get_from_map(soil_to_fertilizer, entity.soil);
+    entity.water       = get_from_map(fertilizer_to_water, entity.fertilizer);
+    entity.light       = get_from_map(water_to_light, entity.water);
+    entity.temperature = get_from_map(light_to_temperature, entity.light);
+    entity.humidity    = get_from_map(temperature_to_humidity, entity.temperature);
+    entity.location    = get_from_map(humidity_to_location, entity.humidity);
+    min_location       = std::min(min_location, entity.location);
+    printf("Seed %ld, soil %ld, fertilizer %ld, water %ld, light %ld, temperature %ld, humidity %ld, location %ld.\n", entity.seed, entity.soil, entity.fertilizer, entity.water, entity.light, entity.temperature, entity.humidity, entity.location);
   }
-
+  
   printf("The lowest location number is: %ld\n", min_location);
   return 0;
 }
