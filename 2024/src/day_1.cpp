ef474bdd9aceb2fd9fdf8218d9b78bef36ed89d6
Marcin Bober <mbober1@gmail.com>
2024-11-27 18:49:45 +0100

    add exceptions for file_read

--- 2024/src/day_1.cpp
+++ 2024/src/day_1.cpp
@@ -1,7 +1,16 @@
 #include <input.hpp>
+#include <iostream>
 
 int main(void)
 {
-  auto file = read_file("day_1.txt");
+  try
+  {
+    auto file = read_file("day_1.txt");
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  
   return 0;
 }
--- utils/input.cpp
+++ utils/input.cpp
@@ -4,7 +4,9 @@
 std::vector<std::string> read_file(const std::string &filename)
 {
   std::vector<std::string> results;
-  std::ifstream file(filename);
+  std::ifstream file;
+  file.exceptions(std::ifstream::failbit);
+  file.open(filename);
 
   std::string line;
   while (std::getline(file, line))
